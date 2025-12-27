cs-soc-tudublin/Plume

commit 82c3660164e629ec1b334ff824c23d96aa4f7d7a

New Pics & optimised pics
Added thumbs for Hero, and lowered res of CMT photos, saving almost 5Mib of Network Traffic.
New photos that are more recent

src/_data/site.yml
@@ -54,80 +54,80 @@ home:
     - name: Lívia Guimarães
       position: 🪑 Chair
       username: livguimaraes
-      image: /assets/img/committee/livnew.webp
+      image: ./assets/img/committee/liv.webp
     - name: Faith Omotayo
       position: 🪑 Vice-Chair
       username: faithomotayo
-      image: /assets/img/committee/faith.webp
+      image: ./assets/img/committee/faith.webp
     - name: Anmool Shah
       position: 💌 Secretary
       username: anmool
-      image: /assets/img/committee/anmool.webp
+      image: ./assets/img/committee/anmool.webp
     - name: It could be you!
       position: 💸 Treasurer
       username: joinUs
-      image: /assets/img/committee/unknown.webp
+      image: ./assets/img/committee/unknown.webp
     - name: Parva Shah
       position: 📢 PRO
       username: ???
-      image: /assets/img/committee/unknown.webp
+      image: ./assets/img/committee/unknown.webp
     - name: Eman Abdelatti
       position: 👩‍⚕️ HSO
       username: Emmy24
-      image: /assets/img/committee/eman.webp
+      image: ./assets/img/committee/eman.webp
     - name: It could be you!
       position: 💻 Tech Events Officer
       username: joinUs
-      image: /assets/img/committee/unknown.webp
+      image: ./assets/img/committee/unknown.webp
     - name: Bohdan Yeriemienko
       position: 🥳 Social Events Officer
       username: bohdan
-      image: /assets/img/committee/bohdan.webp
+      image: ./assets/img/committee/bohdan.webp
     - name: Daniel Smyth
       position: 🚀 SysAdmin
       username: rockerkemm
-      image: /assets/img/committee/dan.webp
+      image: ./assets/img/committee/dan.webp
     - name: D O'Flynn
       position: 🚀 SysAdmin
       username: BigCheese
-      image: /assets/img/committee/derv.webp
+      image: ./assets/img/committee/derv.webp
     - name: MJ
       position: 🚀 SysAdmin
       username: DroolingFeralHog
-      image: /assets/img/committee/mj.webp
+      image: ./assets/img/committee/mj.webp
     - name: Ruán Murgatroyd
       position: 🚀 SysAdmin
       username: rjm
-      image: /assets/img/committee/ruannew.webp
+      image: ./assets/img/committee/ruan.webp
     - name: Tyler Bradley
       position: 🚀 SysAdmin
       username: relyT
-      image: /assets/img/committee/tyler.webp
+      image: ./assets/img/committee/tyler.webp
     - name: It could be you!
       position: 🗓️ Events Rep
       username: joinUs
-      image: /assets/img/committee/unknown.webp
+      image: ./assets/img/committee/unknown.webp
     - name: It could be you!
       position: 👼 First Year Rep
       username: joinUs
-      image: /assets/img/committee/unknown.webp
+      image: ./assets/img/committee/unknown.webp
     - name: It could be you!
       position: 😅 Second Year Rep
       username: joinUs
-      image: /assets/img/committee/unknown.webp
+      image: ./assets/img/committee/unknown.webp
   sponsorSection:
     enquiryStart: Want to be part of something great?
     enquiryEnd: Get in touch!
     enquiryLink: mailto:comp@societies.tudublin.ie?subject=Sponsorship Inquiry
   sponsors:
     - name: Accenture
-      image: /assets/img/sponsors/accenture.png
+      image: ./assets/img/sponsors/accenture.png
       link: https://accenture.com/ie-en
       tier: Primary Sponsor
       weight: bold
       colour: primary
     - name: Rewriting the Code
-      image: /assets/img/sponsors/rtc.webp
+      image: ./assets/img/sponsors/rtc.webp
       link: https://rewritingthecode.org/
       tier: General Sponsor
       weight: normal

src/_layouts/base.njk
@@ -7,8 +7,8 @@ title: CS++
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
-    <link rel="stylesheet" href="/assets/main.css" />
+    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
+    <link rel="stylesheet" href="./assets/main.css" />
     <title>{{ title }}</title>
 
     <meta property="og:type" content="website" >

src/_includes/home/sections/cta/gallery.njk
@@ -1,14 +1,14 @@
 <div class="hero-overlay bg-black bg-opacity-60"></div>
 <div data-zoom class="-z-10 grid grid-cols-12 grid-rows-12 gap-3 size-full">
   <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/handover.webp)]"></div>
-  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/egm2324.webp)]"></div>
-  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/agm2324.webp)]"></div>
-  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/cwg22-sven.webp)]"></div>
-  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/hackathon24.webp)]"></div>
-  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/pycon23.webp)]"></div>
-  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/hackathon22.webp)]"></div>
-  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/cwg22-burger.webp)]"></div>
-  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/coolflip.webp)]"></div>
+  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/egm.webp)]"></div>
+  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/agm.webp)]"></div>
+  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/awards.webp)]"></div>
+  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/fosdem.webp)]"></div>
+  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/pycon.webp)]"></div>
+  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/wistem.webp)]"></div>
+  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/hackathon.webp)]"></div>
+  <div class="gallery-card row-span-4 col-span-4 bg-[url(/assets/img/csball.webp)]"></div>
 </div>
 <script>
   if (!userAgent.getDevice().type) { // is a desktop device

src/_includes/home/sections/hero.njk
@@ -1,5 +1,5 @@
 <section class="relative hero min-h-screen">
-  <img class="absolute inset-0" src="/assets/img/spotlight-bg.webp" alt="" />
+  <img class="absolute inset-0" src="./assets/img/spotlight-bg.webp" alt="" />
   <div class="absolute inset-0 -z-40 bg-base-content [mask-image:url(/assets/svg/grid-bg.svg)]"></div>
   <div class="absolute inset-0 -z-50 bg-base-200"></div>
   <div class="hero-content px-5 py-10 lg:py-20 h-full w-full flex-col md:flex-row">

src/_includes/home/sections/hero/hero-graphic.njk
@@ -1,37 +1,37 @@
 <div class="relative -z-20 perspective-1600 w-full md:w-1/2 aspect-square">
   <img
     class="hero-card bottom-[62%] left-[12%] h-[32%]"
-    src="/assets/img/agm2324.webp"
-    alt="2023 AGM"
+    src="./assets/img/thumbs/agm.webp"
+    alt="2024 AGM"
     data-hero-card
   />
   <img
     class="hero-card bottom-[66%] left-[35%] h-[10%]"
-    src="/assets/img/davidyay.webp"
+    src="./assets/img/davidyay.webp"
     alt="Happy David"
     data-hero-card
   />
   <img
     class="hero-card bottom-[18%] left-[14%] h-[26%]"
-    src="/assets/img/cwg22-sven.webp"
-    alt="CoolFlip"
+    src="./assets/img/thumbs/fosdem.webp"
+    alt="FOSDEM 2025"
     data-hero-card
   />
   <img
     class="hero-card bottom-[12%] left-[58%] h-[22%]"
-    src="/assets/img/hackathon24.webp"
-    alt="Hackathon 2024"
+    src="./assets/img/thumbs/hackathon2.webp"
+    alt="Hackathon 2025"
     data-hero-card
   />
   <img
     class="hero-card bottom-[48%] left-[64%] h-[35%]"
-    src="/assets/img/coolflip.webp"
-    alt="Sven Co-Op Championships"
+    src="./assets/img/thumbs/coolflip.webp"
+    alt="CS++'s Mascot, BitFlip with sunglasses"
     data-hero-card
   />
   <img
     class="absolute bottom-[75%] left-[60%] h-[10%]"
-    src="/assets/img/ruanshock.webp"
+    src="./assets/img/ruanshock.webp"
     alt="Shocked Ruán"
     data-hero-card
   />

commit 7effb36ace8eae795bd36201c7ead62ea1f356c2

Resolved CSS bug in non-root pages
Caused by commit 82c3660, the paths for the favicon and CSS sheets were
made relative instead of absolute, resulting in sub-pages attempting to
load those files from that directory, i.e. `blog/assets/main.css`
instead of the correct `/assets/main.css`.

src/_data/eleventyComputed.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+// const { splitExtension } = require('../../utils/urls')
+
 module.exports = () => {
   const getUrl = (data) => {
     const url = data.page.url

src/_data/site.yml
@@ -5,20 +5,20 @@ society:
   name: CS++
   tagline: TU Dublin's Computer Science Society.
 navigation:
-#  global:
-#    - text: Blog
-#      link: /blog/posts/using-sonarqube.html
-#  blog:
-#    - text: Events
-#      tag: events
-#    - text: Admin Blog
-#      tag: admins
-#    - text: Tutorials
-#      tag: tutorial
-#    - text: Conferences
-#      tag: conferences
-#    - text: Newsletters
-#      tag: newsletter
+  global:
+    - text: Blog
+      link: /blog/posts/using-sonarqube.html
+  blog:
+    - text: Events
+      tag: events
+    - text: Admin Blog
+      tag: admins
+    - text: Tutorials
+      tag: tutorial
+    - text: Conferences
+      tag: conferences
+    - text: Newsletters
+      tag: newsletter
   buttons:
     primary:
       text: Sign Up!

src/_includes/blog/giscus.njk
@@ -5,7 +5,7 @@
         data-repo="cs-soc-tudublin/blog"
         data-repo-id="R_kgDOMQHCxA"
         data-category="Blog"
-        data-category-id="DIC_kwDOLKbwnM4CdBDs"
+        data-category-id="DIC_kwDOMQHCxM4CgfND"
         data-mapping="pathname"
         data-strict="1"
         data-reactions-enabled="1"
@@ -15,7 +15,7 @@
         data-lang="en"
         crossorigin="anonymous"
         async>
-      </script>
+</script>
     </template>
   </is-land>
 </div>

src/_layouts/base.njk
@@ -7,8 +7,8 @@ title: CS++
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico">
-    <link rel="stylesheet" href="./assets/main.css" />
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+    <link rel="stylesheet" href="/assets/main.css" />
     <title>{{ title }}</title>
 
     <meta property="og:type" content="website" >